8ff3e38fa1565603432dd5839289a67ed2307112
conor-egan <egan.conor1@gmail.com>
2017-05-31 16:38:06 +0100

    First commit. List of users returned from dummy API.

---
+++ src/components/test.vue
@@ -0,0 +1,67 @@
+<template>
+
+  <div class="test">
+    <input type="text" v-model="title"><br/>
+    <h1>{{title}}</h1>
+    <p v-if="showName">{{user.firstName}}</p>
+    <p v-else="showName">Nobody</p>
+    <p v-if="showName">{{user.lastName}}</p>
+    <ul>
+      <li v-for="item in items">{{item.title}}</li>
+    </ul>
+    <button v-on:click="greet('Hello World')">Say Greeting</button><br/>
+    <input type="text" v-on:keyup="pressKey" v-on:keyup.enter="enterHit">
+    <hr/>
+    <label>First Name: </label><input type="text" v-model="user.firstName">
+    <br/>
+    <label>Last Name: </label><input type="text" v-model="user.lastName">
+    <h3>{{fullname}}</h3><br/>
+    <h2>{{msg}}</h2>
+  </div>
+</template>
+
+
+<script>
+  export default{
+    name: 'test',
+    props: {
+      msg: {
+        type: String,
+        default: 'Empty message'
+      }
+    },
+    data(){
+      return {
+        title: 'Hello World',
+        user: {
+          firstName: "John",
+          lastName: 'Doe'
+        },
+        showName: true,
+        items: [
+        {title: 'item1'},{title: 'item2'},{title: 'item3'}
+        ]
+      }
+    },
+    methods: {
+      greet: function(greeting){
+        alert(greeting);
+      },
+      pressKey: function(e){
+        console.log(e.target.value);
+      },
+      enterHit: function(){
+        console.log('You hit enter');
+      }
+    },
+    computed: {
+      fullname: function(){
+        return this.user.firstName + ' '+this.user.lastName;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
